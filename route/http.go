ec05e6fa1e8d2ea457f44aaf5c39549d88501503
xotam <fobus1289@mail.ru>
2021-03-21 17:34:22 +0500

    add

--- route/http.go
+++ route/http.go
@@ -4,18 +4,8 @@ import (
 	"net/http"
 )
 
-func Get(url string) *route {
-
-	rou := new(route)
-
-	http.HandleFunc(url, func(writer http.ResponseWriter, request *http.Request) {
-
-	})
-
-	return rou
-}
-
 func valid(writer http.ResponseWriter, request *http.Request, rou *route, method string, client Client) bool {
+
 	if request.Method != method {
 		writer.WriteHeader(405)
 		_, _ = writer.Write([]byte("Method Not Allowed "))
@@ -37,14 +27,15 @@ func valid(writer http.ResponseWriter, request *http.Request, rou *route, method
 	return true
 }
 
-func Post(url string, action func(client *Client)) *route {
+func handleFunc(url string, method string, action func(client *Client)) *route {
 	rou := new(route)
 
 	go http.HandleFunc(url, func(writer http.ResponseWriter, request *http.Request) {
 		client := new(Client)
 		client.request = request
 		client.response = &writer
-		if !valid(writer, request, rou, "POST", *client) {
+
+		if !valid(writer, request, rou, method, *client) {
 			return
 		}
 
@@ -54,32 +45,22 @@ func Post(url string, action func(client *Client)) *route {
 	return rou
 }
 
-func PostNoRet(url string, action func(writer http.ResponseWriter, request *http.Request)) *route {
-	rou := new(route)
-
-	go http.HandleFunc(url, func(writer http.ResponseWriter, request *http.Request) {
-		action(writer, request)
-	})
-
-	return rou
+func Get(url string, action func(client *Client)) *route {
+	return handleFunc(url, "GET", action)
 }
 
-func Put(url string, action func(client interface{})) {
-
+func Post(url string, action func(client *Client)) *route {
+	return handleFunc(url, "POST", action)
 }
 
-func Patch(url string, action interface{}) {
-
+func Put(url string, action func(client *Client)) *route {
+	return handleFunc(url, "PUT", action)
 }
 
-func Delete(url string, action interface{}) {
-
+func Patch(url string, action func(client *Client)) *route {
+	return handleFunc(url, "PATCH", action)
 }
 
-func New(response http.ResponseWriter, request *http.Request) *Client {
-
-	return &Client{
-		&response,
-		request,
-	}
+func Delete(url string, action func(client *Client)) *route {
+	return handleFunc(url, "DELETE", action)
 }
